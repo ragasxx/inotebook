2b72efa035218dc6491db21a46c4180d54b004d7
Sagar Bhatia <sagarbhatia722@gmail.com>
2022-02-06 15:15:13 +0530

    api calls

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,24 +1,153 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import noteContext from "../context/notes/noteContext";
 import AddNote from "./AddNote";
 import Noteitem from "./Noteitem";
 
 const Notes = () => {
   const context = useContext(noteContext);
-  const { notes, getnotes } = context;
+  const { notes, getnotes, editNote } = context;
 
   useEffect(() => {
     getnotes();
   }, [getnotes]);
 
+  const ref = useRef(null);
+  const refClose = useRef(null);
+
+  const [note, setNote] = useState({
+    id: "",
+    etitle: "",
+    edescription: "",
+    etag: "",
+  });
+
+  const updateNote = (currentNote) => {
+    ref.current.click();
+    setNote({
+      id: currentNote._id,
+      etitle: currentNote.title,
+      edescription: currentNote.description,
+      etag: currentNote.tag,
+    });
+  };
+
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+  };
+
+  const handleclick = (e) => {
+    console.log("updating the note", note);
+    editNote(id, etitle, edescription, etag);
+    refClose.current.click();
+  };
+
   return (
     <>
       <AddNote />
+
+      <button
+        ref={ref}
+        type="button"
+        className="btn btn-primary d-none "
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                UPDATE YOUR NOTE HERE...
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="etitle"
+                    name="etitle"
+                    value={note.etitle}
+                    aria-describedby="emailHelp"
+                    onChange={onChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="description" className="form-label">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="edescription"
+                    name="edescription"
+                    value={note.edescription}
+                    onChange={onChange}
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="tag" className="form-label">
+                    TAG
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="etag"
+                    name="etag"
+                    value={note.etag}
+                    onChange={onChange}
+                    autoComplete="off"
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                ref={refClose}
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleclick}
+              >
+                Update Note
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="row my-3">
         <h2 className="my-4 mx-4">YOUR NOTES</h2>
 
         {notes.map((note) => {
-          return <Noteitem key={note._id} note={note} />;
+          return (
+            <Noteitem updateNote={updateNote} key={note._id} note={note} />
+          );
         })}
       </div>
     </>
